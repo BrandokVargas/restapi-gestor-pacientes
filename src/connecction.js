1d4473a170ef021fbc276453c683a1e372fab2a0
BrandokVargas <brandokvargas27@gmail.com>
2023-02-01 17:12:04 -0500

    Agregando variable password

--- src/connecction.js
+++ src/connecction.js
@@ -1,10 +1,10 @@
 import { createPool } from "mysql2/promise";
-import { DB_DATABASE,DB_HOST,DB_PORT,DB_USER } from "./config.js";
+import { DB_DATABASE,DB_HOST,DB_PORT,DB_USER ,DB_PASSWORD} from "./config.js";
 
 export const connecction = createPool({
     host: DB_HOST,
     user: DB_USER,
-    password: '',
+    password: DB_PASSWORD,
     port: DB_PORT,
     database: DB_DATABASE  
 })
